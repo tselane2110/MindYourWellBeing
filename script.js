580adbada8fe04b43dafe8e76fab17e42d332872
TANZEELA S <muc.555@gmail.com>
2023-11-28 10:27:02 +0500

    updated files

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fetch and inject header
+fetch('base-templates/head.html')
+.then(response => response.text())
+.then(html => document.getElementById('head').innerHTML = html);
+
+// Fetch and inject footer
+fetch('base-templates/footer.html')
+.then(response => response.text())
+.then(html => document.getElementById('footer').innerHTML = html);
+
