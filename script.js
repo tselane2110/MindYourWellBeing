cbbc8cfb79ddedafe019f1246619fdb5f74ca94f
TANZEELA S <muc.555@gmail.com>
2024-01-04 14:02:20 +0500

    fix: form submission

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ fetch('base-templates/footer.html')
 function subscribeNewsletter(e){
     e.preventDefault();
 
-    let s_email = document.getElementById("subscribe_email").value;
+    let s_email = document.getElementById("subscribe_email");
 
     alert("you subscribed!");
-    console.log(`${s_email} has subscribed!`);
+    console.log(`${s_email.value} has subscribed!`);
+
+    s_email.value = "";
 }
 
 // getting data from appointment.html -- user input data
@@ -24,22 +26,22 @@ function subscribeNewsletter(e){
 function submitLoginForm(e){
     e.preventDefault();
 
-    let full_name = document.getElementById("fullName").value;
-    let email = document.getElementById("email").value;
-    let phone_number = document.getElementById("phone").value;
-    let date = document.getElementById("appointmentDate").value;
-    let message = document.getElementById("message").value;
+    let full_name = document.getElementById("fullName");
+    let email = document.getElementById("email");
+    let phone_number = document.getElementById("phone");
+    let date = document.getElementById("appointmentDate");
+    let message = document.getElementById("message");
 
     // perform operation with form input
     alert("This form has been successfully submitted!");
-    console.log(`The user's name is ${full_name} and email is ${email}`);
+    console.log(`The user's name is ${full_name.value} and email is ${email.value}`);
 
     // changing values back to placeholders
 
-    full_name = "";
-    email = "";
-    phone_number = "";
-    date = "";
-    message = "";
+    full_name.value = "";
+    email.value = "";
+    phone_number.value = "";
+    date.value = "";
+    message.value = "";
 }
 
