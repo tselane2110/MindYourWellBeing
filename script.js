dc8fb90ec7571ef538c88a1d42c91ef869e4c207
TANZEELA S <muc.555@gmail.com>
2024-01-04 13:58:32 +0500

    fix : form-submission

--- script.js
+++ script.js
@@ -8,4 +8,38 @@ fetch('base-templates/footer.html')
 .then(response => response.text())
 .then(html => document.getElementById('footer').innerHTML = html);
 
+// getting emails for newsletter
+
+function subscribeNewsletter(e){
+    e.preventDefault();
+
+    let s_email = document.getElementById("subscribe_email").value;
+
+    alert("you subscribed!");
+    console.log(`${s_email} has subscribed!`);
+}
+
 // getting data from appointment.html -- user input data
+
+function submitLoginForm(e){
+    e.preventDefault();
+
+    let full_name = document.getElementById("fullName").value;
+    let email = document.getElementById("email").value;
+    let phone_number = document.getElementById("phone").value;
+    let date = document.getElementById("appointmentDate").value;
+    let message = document.getElementById("message").value;
+
+    // perform operation with form input
+    alert("This form has been successfully submitted!");
+    console.log(`The user's name is ${full_name} and email is ${email}`);
+
+    // changing values back to placeholders
+
+    full_name = "";
+    email = "";
+    phone_number = "";
+    date = "";
+    message = "";
+}
+
