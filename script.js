d3f0f60d4ba8349ed844134a30a92183ee74e1e5
TANZEELA S <muc.555@gmail.com>
2024-01-08 22:22:41 +0500

    add: pop-up modal

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function subscribeNewsletter(e){
     s_email.value = "";
 }
 
+
 // getting data from appointment.html -- user input data
 
 function submitLoginForm(e){
@@ -34,7 +35,18 @@ function submitLoginForm(e){
 
     // perform operation with form input
     alert("This form has been successfully submitted!");
-    console.log(`The user's name is ${full_name.value} and email is ${email.value}`);
+
+    //form-data object: 
+
+    const form_data = {
+        "full_name": full_name.value,
+        "email" : email.value,
+        "phone_number" :phone_number.value,
+        "date" : date.value,
+        "message" : message.value 
+    }
+
+    console.log(form_data);
 
     // changing values back to placeholders
 
@@ -45,3 +57,17 @@ function submitLoginForm(e){
     message.value = "";
 }
 
+// get-help popup box
+
+function gethelp(e){
+    e.preventDefault();
+    var modal = document.getElementById('helpModal');
+    modal.style.display = "block";
+    console.log("i got clicked!");
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+          modal.style.display = 'none';
+        }
+      });
+}
